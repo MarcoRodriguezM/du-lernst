c06cb30e39c4e48c216a9189533f4ad18882019d
kristhelcorea <kristhellcorea@gmail.com>
2020-11-13 14:05:27 -0600

    Restablecer contraseña,  verificar si el usuario existe, direccionamiento de correo, redireccionar al inicio.

--- controllers/authController.js
+++ controllers/authController.js
@@ -29,3 +29,83 @@ exports.cerrarSesion = (req, res, next) => {
   
     return res.redirect("/iniciar-sesion");
   };
+  // Mostrar el formulario de restablecer la contraseña
+exports.formularioRestablecerPassword = (req, res, next) => {
+  // console.log(res.locals.messages.messages[0].messages[0].message);
+  res.render("restablecerPassword", {
+    // layout: "auth",
+    typePage: "register-page",
+    signButtonValue: "/iniciar-sesion",
+    signButtonText: "Iniciar sesión",
+    year: new Date().getFullYear(),
+  });
+};
+
+// Enviamos un token de autenticación al usuario para cambiar su
+// contraseña. El token se envía al correo del usuario.
+exports.enviarToken = async (req, res, next) => {
+  // Obtener la direccción de correo electrónico
+  const { email } = req.body;
+  const messages = [];
+
+  // Buscar el usuario
+  try {
+    const usuario = await Usuario.findOne({ email });
+
+    // El usuario no existe
+    if (!usuario) {
+      messages.push({
+        message:
+          "¡No existe una cuenta registrado con este correo electrónico!",
+        alertType: "danger",
+      });
+
+      req.flash("messages", messages);
+
+      res.redirect("/olvide-password");
+    }
+
+    // El usuario existe, generar un token y una fecha de vencimiento
+    usuario.token = crypto.randomBytes(20).toString("hex");
+    usuario.expira = Date.now() + 3600000;
+
+    // Guardar los cambios
+    await usuario.save();
+
+    // Generar la URL de restablecer contraseña
+    const resetUrl = `http://${req.headers.host}/olvide-password/${usuario.token}`;
+
+    try {
+      // Enviar la notificación al correo electrónico del usuario
+      const sendMail = await enviarCorreo.enviarCorreo({
+        to: usuario.email,
+        subject: "Restablece tu contraseña en Cashize",
+        template: "resetPassword",
+        nombre: usuario.nombre,
+        resetUrl,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+
+    // Redireccionar al inicio de sesión
+    messages.push({
+      message: "¡Verifica tu bandeja de entrada y sigue las instrucciones!",
+      alertType: "success",
+    });
+
+    req.flash("messages", messages);
+
+    res.redirect("/olvide-password");
+  } catch (error) {
+    messages.push({
+      message:
+        "Ocurrió un error al momento de comunicarse con el servidor. Favor intentar nuevamente.",
+      alertType: "danger",
+    });
+
+    req.flash("messages", messages);
+
+    res.redirect("/olvide-password");
+  }
+};
