343e4752e0a04f621e772b50a45f3f2d5a43ef76
kristhelcorea <kristhellcorea@gmail.com>
2020-11-13 13:23:46 -0600

    cerrar sesion usuario

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,3 +13,19 @@ exports.autenticarUsuario = passport.authenticate("local", {
   failureFlash: true,
   badRequestMessage: ["Debes ingresar tus credenciales"],
 });
+// Cerrar la sesión del usuario
+exports.cerrarSesion = (req, res, next) => {
+    const messages = [];
+  
+    // Cierra la sesión
+    req.logout();
+  
+    messages.push({
+      message: "Has cerrado correctamente tu sesión. ¡Vuelve pronto!",
+      alertType: "success",
+    });
+  
+    req.flash("messages", messages);
+  
+    return res.redirect("/iniciar-sesion");
+  };
