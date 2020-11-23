510df2e19c8dc92c2ccf5280dec27dd19aca07af
MarcoRodriguezM <57971630+MarcoRodriguezM@users.noreply.github.com>
2020-11-23 13:48:50 -0600

    modelo video

---
+++ models/Video.js
@@ -0,0 +1,35 @@
+// Importar los módulos requeridos
+const mongoose = require("mongoose");
+const shortid = require("shortid");
+
+// Definición del schema
+const videoSchema = new mongoose.Schema({
+  nombre: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  descripcion: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  video: String,
+  url: {
+    type: String,
+    lowercase: true,
+  },
+});
+// Hooks para generar la URL de la categoria
+videoSchema.pre("save", function (next) {
+  // Crear la URL
+  const url = slug(this.nombre);
+  this.url = `${url}-${shortid.generate()}`;
+
+  next();
+});
+
+// Generar un índice para mejorar la búsqueda por el nombre de la categoria
+videoSchema.index({ nombre: "text" });
+
+module.exports = mongoose.model("Video", videoSchema);
