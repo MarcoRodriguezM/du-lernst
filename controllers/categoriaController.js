8febde9d08015c4aa1a26522bd69356464f6e5a1
MarcoRodriguezM <57971630+MarcoRodriguezM@users.noreply.github.com>
2020-12-15 17:09:15 -0600

    correcion categorias

--- controllers/categoriaController.js
+++ controllers/categoriaController.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const Categoria = mongoose.model("Categoria");
 const { validationResult } = require("express-validator");
 const authController = require("../controllers/authController");
+const multer = require("multer");
+const shortid = require("shortid");
 
 exports.mostrarCategorias = async (req, res, next) => {
     const categorias = await Categoria.find().lean();
@@ -36,7 +38,7 @@ exports.formularioCrearCategoria = (req, res, next) => {
     // Verificar que no existen errores de validación
     const errores = validationResult(req);
     const messages = [];
-  
+    console.log(req.body);
     // Si hay errores
     if (!errores.isEmpty()) {
       errores.array().map((error) => {
@@ -55,6 +57,7 @@ exports.formularioCrearCategoria = (req, res, next) => {
         await Categoria.create({
           nombre,
           descripcion,
+          imagen: req.file.filename
         });
   
         messages.push({
@@ -75,3 +78,77 @@ exports.formularioCrearCategoria = (req, res, next) => {
       }
     }
   };
+
+  exports.subirImagen = (req, res, next) => {
+  
+    upload(req, res, function (error) {
+      console.log(req.body);
+      if (error) {
+        // Errores de Multer
+        if (error instanceof multer.MulterError) {
+          if (error.code === "LIMIT_FILE_SIZE") {
+            req.flash("messages", [
+              {
+                message:
+                  "El tamaño del archivo es superior al límite. Máximo 300Kb",
+                alertType: "danger",
+              },
+            ]);
+          } else {
+            req.flash("messages", [
+              { message: error.message, alertType: "danger" },
+            ]);
+          }
+        } else {
+          // Errores creado por el usuario
+          req.flash("messages", [
+            { message: error.message, alertType: "danger" },
+          ]);
+        }
+        // Redireccionar y mostrar el error
+        res.redirect("/crear-categoria");
+        return;
+      } else {
+        // Archivo cargado correctamente
+        return next();
+      }
+    });
+    // }
+  };
+
+  const configuracionMulter = {
+    // Tamaño máximo del archivo en bytes
+    limits: {
+      fileSize: 100000000,
+    },
+    // Dónde se almacena el archivo
+    storage: (fileStorage = multer.diskStorage({
+      destination: (req, res, cb) => {
+        cb(null, `${__dirname}../../public/uploads/categorias`);
+      },
+      filename: (req, file, cb) => {
+        // Construir el nombre del archivo
+        // iphone.png --> image/png --> ["image", "png"]
+        // iphone.jpg --> image/jpeg
+        const extension = file.mimetype.split("/")[1];
+        cb(null, `${shortid.generate()}.${extension}`);
+      },
+    })),
+    // Verificar el tipo de archivo mediante el mime type
+    // https://developer.mozilla.org/es/docs/Web/HTTP/Basics_of_HTTP/MIME_types
+    fileFilter(req, file, cb) {
+      if (file.mimetype === "image/png" || file.mimetype === "image/jpeg") {
+        // Si el callback retorne true se acepta el tipo de archivo
+        cb(null, true);
+      } else {
+        cb(
+          new Error(
+            "Formato de archivo no válido. Solamente se permniten JPEG/JPG o PNG"
+          ),
+          false
+        );
+      }
+    },
+  };
+
+  const upload = multer(configuracionMulter).single("imagen");
